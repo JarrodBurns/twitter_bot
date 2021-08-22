d85187b0b78ee531a91d6f641dd5a81277a9cbe9
JarrodBurns <87616660+JarrodBurns@users.noreply.github.com>
2021-08-22 16:37:42 -0500

    Minor tweaks
    Adjusted formatting slightly.

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,5 +1,5 @@
 
-from hidden import consumer_key, consumer_secret, access_token, access_token_secret, email, password, mail_to
+from hidden import *
 import tweepy
 import time
 import yagmail
@@ -13,7 +13,7 @@ api = tweepy.API(auth, wait_on_rate_limit=True,
 
 FILE_NAME = "last_seen.txt"
 
-hastag = "#respondtome"
+hashtag = "#respondtome"
 hashtag_message = "Twitter bot reporting in!"
 no_hashtag_message = ("Jarrod has been forwarded a copy of your message. "
                       "Use #respondtome to have BotJarrod generate more "
@@ -22,7 +22,7 @@ no_hashtag_message = ("Jarrod has been forwarded a copy of your message. "
 subject = "Twitter bot has detected a message."
 
 
-def send_email(mail_to: str, subject: str, content: str):
+def send_email(mail_to: str, subject: str, content: str) -> None:
     """
     Sends an email to the specified account (mail_to) with
     a specified message (content).
@@ -30,7 +30,7 @@ def send_email(mail_to: str, subject: str, content: str):
     yagmail.SMTP({email: "Twitter Bot"}, password).send(mail_to, subject, content)
 
 
-def read_last_seen(FILE_NAME: str):
+def read_last_seen(FILE_NAME: str) -> str:
     """
     Checks the ID of the last seen comment.
     """
@@ -40,19 +40,18 @@ def read_last_seen(FILE_NAME: str):
     return last_seen_id
 
 
-def store_last_seen(FILE_NAME: str, last_seen_id: str):
+def store_last_seen(FILE_NAME: str, last_seen_id: str) -> None:
     """
     Saves the ID of the last seen comment.
     """
     file_write = open(FILE_NAME, "w")
     file_write.write(str(last_seen_id))
     file_write.close()
-    return
 
 
-def reply(hastag: str):
+def reply(hashtag: str) -> None:
     """
-    Looks for messages, checks for a specified hashtag and replys to users
+    Looks for messages, checks for a specified hashtag, and replys to users.
     """
     tweets = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode="extended")
 
@@ -60,7 +59,7 @@ def reply(hastag: str):
 
         content = f"Message from: @{tweet.user.screen_name}\n\n{tweet.full_text}"
 
-        if hastag in tweet.full_text.lower():
+        if hashtag in tweet.full_text.lower():
             # Reply
             api.update_status(f"@{tweet.user.screen_name} {hashtag_message}", tweet.id)
             # Share
@@ -73,6 +72,7 @@ def reply(hastag: str):
         api.create_favorite(tweet.id)  # like
         api.create_friendship(tweet.user.screen_name)  # Sub
         store_last_seen(FILE_NAME, tweet.id)
+
         print(f"---Now following {tweet.user.screen_name}")
         print(f"{tweet.id} - {tweet.full_text}\n")
 
@@ -85,12 +85,13 @@ def current_time() -> str:
     return datetime.now().strftime('%m-%d-%Y @ %H:%M:%S')
 
 
-print(f"Initializing on {current_time()}.\n")
+print(f"\nInitializing on {current_time()}.\n")
 
 count = 0
 while True:
-    reply(hastag)
+    reply(hashtag)
     time.sleep(60)  # 60 is one minute.
     count += 1
     if count % 60 == 0:  # 60 is one hour. 1440 is one day.
-        print(f"Connection valid on {current_time()}. Request ID: {count} Report ID: {int(count / 60)}\n")
+        print(f"Connection valid on {current_time()}. "
+              f"Request ID: {count} Report ID: {int(count / 60)}")
